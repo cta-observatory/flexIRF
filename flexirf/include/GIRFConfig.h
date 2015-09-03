c551c9424c857bfe48bc62d51a950e9da123f411
tialis <goldstein2034@hotmail.com>
2015-09-03 12:17:27 +0200

    - GIRFConfig added.

---
+++ flexirf/include/GIRFConfig.h
@@ -0,0 +1,68 @@
+#ifndef G_IRFConfig
+#define G_IRFConfig
+
+#include <string>
+
+class GIRFConfig {
+public:
+
+	//////////////////////////////////////////////////////////
+	//						TODO
+	//			Ranges need to be added!!!
+	//	The GIRF.GetPdf should select the ranges they want.
+	//
+	//////////////////////////////////////////////////////////
+	enum MolProfile {
+		kNoMolProfile = 0,
+		kAverage,
+		kSummer,
+		kWinter,
+		kSpring,
+		kAutumn,
+		kMolProfileMax
+	};
+
+	enum NSBLevel {
+		kNoNSBLevel = 0, kExtragalactic, kGalactic, kNSBLevelMax
+	};
+
+	enum ObsMode {
+		kNoObsMode = 0, kPoint, kDivergent, kObsModeMax
+	};
+	enum SubArray {
+		kNoSubArray = 0, kFull, kLSTs, kMSTSSTs, kSubArrayMax
+	};
+	enum Trigger {
+		kNoTrigger = 0, kStandard, kHighDTs, kTriggerMax
+	};
+	struct HWStatus {
+		float reflectivity;
+		Trigger trigger;
+	};
+
+	struct Weather {
+		float Transmitance;
+		MolProfile molProfile;
+	};
+
+	// TODO
+	// Add Event selection... Pointing direction... Etc....
+
+private:
+	Weather fWeather;
+	NSBLevel fNSBLevel;
+	ObsMode fObsMode;
+	SubArray fSubArray;
+	HWStatus fHWStatus;
+	int LoadConfigFile(std::string filename);
+
+public:
+
+	GIRFConfig();
+	GIRFConfig(std::string filename);
+	virtual ~GIRFConfig() {};
+	void SetDefault();
+};
+
+#endif
+
